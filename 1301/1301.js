406371cdc15338a3723f9832e00be52c7f7b4c69
unknown <cda609@AFPA-PARIS-DI>
2021-01-13 17:07:07 +0100

    bases js

--- 1301/1301.js
+++ 1301/1301.js
@@ -53,26 +53,29 @@ do{
 */
 //Tableaux
 
-var fruits = ["bananes", "oranges", "fraise", "peche"];
+var fruits = ["oranges", "bananes", "oranges", "fraise", "peche"];
 for (ligne = 0; ligne < fruits.length; ligne++){
-	console.log("ligne " + (ligne+1));
+	console.log("for ligne " + (ligne+1));
 	console.log(fruits[ligne]);
 }
 var ligne = -1;
 do{
 	ligne ++;
+	console.log("do while recherche ligne " + (ligne+1));
 	console.log(fruits[ligne]);
 	console.log("J'aime les " + fruits[ligne]);
-} while(fruits[ligne] != "bananes")
-console.log("fin do while recherche bananes");
+} while(fruits[ligne] != "fraise")
+
 ligne = -1;
 do{
 	ligne ++;
+	console.log("do while total ligne " + (ligne+1));
 	console.log("J'aime les " + fruits[ligne]);
 } while(ligne < fruits.length-1)
-console.log("fin do while affichage tableau");
+
 ligne = 0
 while(ligne < fruits.length){
+	console.log("while total ligne " + (ligne+1));
 	console.log("J'aime les " + fruits[ligne]);
 	ligne++;
 }
