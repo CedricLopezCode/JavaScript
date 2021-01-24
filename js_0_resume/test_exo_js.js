060627c08151578b0fd070e0ef995e8c3c27ea10
Cedric <cedric-lopez@hotmail.fr>
2021-01-24 19:51:57 +0100

    essai

--- js_0_resume/test_exo_js.js
+++ js_0_resume/test_exo_js.js
@@ -3,6 +3,8 @@ function lancer_js_exo(script_a_lancer){
 	console.log(script_sans_src);
 	var nom_exo = trouver_nom_function(script_a_lancer);
 	console.log(nom_exo);
+	nom_exo;
+	document.onload = nom_exo;
 }
 function changement_src(script_a_lancer){
 	var script_a_lancer = script_a_lancer.getAttribute("src");
@@ -11,7 +13,7 @@ function changement_src(script_a_lancer){
 }
 function trouver_nom_function(script_a_lancer){
 	var script_a_lancer = script_a_lancer.getAttribute("src");
-	var regex = /[0-9a-z_]+\.js/ 
+	var regex = /[0-9a-z_]+.js/ 
 	nom_function_avec_extension = (script_a_lancer.match(regex)).toString();
 	nom_function_sans_extension = nom_function_avec_extension.slice(0,-3);
 	return nom_function_sans_extension;
