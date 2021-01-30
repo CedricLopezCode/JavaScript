6ecd33a5399228307b653ad28261f731f971b090
Cedric <cedric-lopez@hotmail.fr>
2021-01-30 13:23:33 +0100

    Liste étudiants et TVA

--- js_2801/liste_etudiants.js
+++ js_2801/liste_etudiants.js
@@ -44,13 +44,76 @@ tableau_des_etudiants.length == 0 ? document.querySelector("#liste_etudiant").in
 //-------------------------------------------------------------
 //-------------------------------------------------------------
 //Correction
-if(tableau_des_etudiants.length == 0){
+//creation et affichage tableau
+/*if(tableau_des_etudiants.length == 0){
 	document.write("Pas d’étudiants inscrits pour le moment")
 }
 else{
 	var liste_etudiant_correction = document.createElement("table");
 	liste_etudiant_correction.setAttribute("class", "table table-striped")
 }
+var titre_cor = document.createElement("h2");
+titre_cor.appendChild(createTextNode(Liste des étudiants));
+document.querySelector(".container").append(titre_cor)
+//bouton admis
+var bouton_admis_cor = document.createElement("button");
+bouton_admis_cor.setAttribute("class", "btn btn-success");
+contenu_bouton_admis_cor = document.createTextNode("Bouton des admis");
+var badge_admis = document.createElement("span");
+badge_admis.setAttribute("class", "badge bg-secondary");
+var contenu_badge_admis = document.createTextNode(compteur_admis)
+badge_admis.appendChild(contenu_badge_admis)
+bouton_admis_cor.appendChild(contenu_bouton_admis_cor);
+bouton_admis_cor.appendChild(badge_admis);
+document.querySelector("container").append(bouton_admis_cor);
+//bouton refuse
+var bouton_refuse_cor = document.createElement("button");
+bouton_refuse_cor.setAttribute("class", "btn btn-success admis");
+contenu_bouton_refuse_cor = document.createTextNode("Bouton des refuse");
+var badge_refuse = document.createElement("span");
+badge_refuse.setAttribute("class", "badge bg-secondary refuse");
+var contenu_badge_refuse = document.createTextNode(compteur_refuse)
+badge_refuse.appendChild(contenu_badge_refuse)
+bouton_refuse_cor.appendChild(contenu_bouton_refuse_cor);
+bouton_refuse_cor.appendChild(badge_refuse);
+document.querySelector("container").append(bouton_refuse_cor);
+//Liste admis ou refuse
+function listeAdmis(){
+	var ul_admis = document.createElement("ul");
+	for (index = 0; index < tableau_des_etudiants.length; index++){
+		if(tableau_des_etudiants.moyenne >= 10){
+			var li_admis = document.createElement("td");
+			var prenom_cor = document.createTextNode(tableau_des_etudiants[index].prenom);
+			var nom_cor = document.createTextNode(tableau_des_etudiants[index].prenom);
+			var moyenne_corr = document.createTextNode(" avec " +tableau_des_etudiants[index].prenom + " de moyenne");
+			
+			li_admis.appendChild(prenom_cor);
+			li_admis.appendChild(nom_cor);
+			li_admis.appendChild(moyenne_corr);
+			ul_admis.appendChild(li_admis);
+		}
+		document.querySelectorAll("ul").length>0 ? document.querySelector("ul").remove() : "";
+		document.querySelector(".bouton_admis_cor").append(ul_admis);
+	}
+}
+function listeRefuse(){
+	var ul_refuse = document.createElement("ul");
+	for (index = 0; index < tableau_des_etudiants.length; index++){
+		if(tableau_des_etudiants.moyenne < 10){
+			var li_refuse = document.createElement("td");
+			var prenom_cor = document.createTextNode(tableau_des_etudiants[index].prenom);
+			var nom_cor = document.createTextNode(tableau_des_etudiants[index].prenom);
+			var moyenne_corr = document.createTextNode(" avec " +tableau_des_etudiants[index].prenom + " de moyenne");
+			
+			li_refuse.appendChild(prenom_cor);
+			li_refuse.appendChild(nom_cor);
+			li_refuse.appendChild(moyenne_corr);
+			ul_refuse.appendChild(li_refuse);
+		}
+		document.querySelectorAll("ul").length>0 ? document.querySelector("ul").remove() : "";
+		document.querySelector(".bouton_refuse_cor").append(ul_admis);
+	}
+}*/
 //---------------------------------------------------------------
 //---------------------------------------------------------------
 var tableau_html = document.querySelector("#tableau_liste_etudiant");
@@ -123,20 +186,21 @@ var bouton_admis = document.querySelector("#bouton_admis");
 bouton_admis.onclick = afficher_les_moyennes_admis;
 var bouton_refuse = document.querySelector("#bouton_refuse");
 bouton_refuse.onclick = afficher_les_moyennes_refuse;
+
 function afficher_les_moyennes_admis(){
 	document.querySelector("#liste_etudiant").appendChild(liste_admis);
+	document.querySelector("#liste_etudiant").replaceChild(liste_admis , liste_refuse);
 	
 	//A voir avec moussa pour que cliquer sur l'un fasse disparaitre la liste de l'autre
-	bouton_refuse.addEventListener("click", function(event){
-	event.preventDefault();
-	});
+	//pas possible avec des events
+	bouton_refuse.addEventListener("click", function(event){event.preventDefault();});
 }
 function afficher_les_moyennes_refuse(){
 	document.querySelector("#liste_etudiant").appendChild(liste_refuse);
+	document.querySelector("#liste_etudiant").removeChild(liste_admis);
 	//A voir avec moussa pour que cliquer sur l'un fasse disparaitre la liste de l'autre
-	bouton_admis.addEventListener("click", function(event){
-	event.preventDefault();
-	});
+	//pas possible avec des events
+	bouton_admis.addEventListener("click", function(e){e.preventDefault();});
 }
 
 //------------------------------------------------------------------
